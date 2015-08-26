d9ce01bf8811496ca67c3175c007c82b1477021a
Aniruddh Agarwal <aaniruddh99@gmail.com>
2015-08-26 18:35:23 +0800

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://minterest:minterest@ds035553.mongolab.com:35553/minterest'
+  },
+
+  seedDB: true
+};
---
+++ server/config/environment/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://minterest:minterest@ds035553.mongolab.com:35553/minterest'
+  }
+};
