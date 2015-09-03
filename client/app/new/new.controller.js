a2fe9924b7737abf809e6d9ec6b93d2d5e102cc1
Aniruddh Agarwal <aaniruddh99@gmail.com>
2015-09-03 21:20:49 +0800

    added placeholder image

--- client/app/new/new.controller.js
+++ client/app/new/new.controller.js
@@ -2,12 +2,12 @@
 
 angular.module('minterestApp')
   .controller('NewCtrl', function ($scope, $http, Auth, $location) {
-    $scope.mint = { username: Auth.getCurrentUser().name };
+    $scope.mint = { user: Auth.getCurrentUser().name };
 
     $scope.submitMint = function() {
         $http.post('/api/mints', $scope.mint).
         then(function(response) {
-            console.log(response);
+            $location.path('/');
         });
     }
   });
